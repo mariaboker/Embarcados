3a511ed815f18f05defa577448893fdfeed5f1d1
Maria <mariaboker@hotmail.com>
2019-05-07 22:06:21 -0300

    Update 076_lab02.c

--- 076_lab02.c
+++ 076_lab02.c
@@ -19,6 +19,16 @@
 #include <SoftwareSerial.h>
 SoftwareSerial serial (0, 1); //pinos 0 e 1 para rx e tx
 
+/// #include "SoftwareSerial.h"   // para bluetooth
+/// SoftwareSerial bluetooth(2, 3);   // TX e RX usadas para modulo bluetooth
+
+// #include <Adafruit_GFX.h>
+
+//#include <Adafruit_PCD8544.h>
+//Adafruit_PCD8544 display = Adafruit_PCD8544(8, 9, 10, 11, 12);
+
+#include <Wire.h>   // para I2C
+
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 13, 4, 5, 6, 7);
 //RS - pino 12
@@ -28,26 +38,16 @@ LiquidCrystal lcd(12, 13, 4, 5, 6, 7);
 //DB5 - pino 5
 //DB4 - pino 4
 
-#include <Adafruit_GFX.h>
 
-#include <Adafruit_PCD8544.h>
-Adafruit_PCD8544 display = Adafruit_PCD8544(8, 9, 10, 11, 12);
-
-/// #include "SoftwareSerial.h"   // para bluetooth
-/// SoftwareSerial bluetooth(2, 3);   // TX e RX usadas para modulo bluetooth
-
-#include <Wire.h>   // para I2C
-
-
-string input; // string lida na entrada serial
+String input; // string lida na entrada serial
 char last; // ultimo caractere lido na entrada serial
 int nchar = 0; // numero de caracteres lidos
 int rpm; // valor estimado para velocidade em rpm
 int pinH0 = 10;   // pinos 10 PWM
 int pinH1 = 11;   // pinos 11 PWM
 int pinIn = 9; // pino de input do sensor de luz do motor
-int pinPWM = ;  // enable, Usa pwm sim pcausa do setvel - arranjar porta ?????????
-char velo[3]; // string that will receive the new velo
+int pinPWM;  // enable, Usa pwm sim pcausa do setvel - arranjar porta ?????????
+String velo; // string that will receive the new velo
 
 
 
@@ -61,6 +61,31 @@ void configuracao_Timer0(){
 }
 
 
+
+/***    ***/
+
+void setup(){
+  Serial.begin(9600);   // taxa de transmissao serial
+  /// bluetooth.begin(9600);
+
+  // Seta como output os pinos de pwm e pinos ponte H
+  pinMode(pinPWM, OUTPUT);
+  pinMode(pinH1, OUTPUT);
+  pinMode(pinH0, OUTPUT);
+
+  // Deixa parado inicialmente
+  digitalWrite(pinH0, LOW);
+  digitalWrite(pinH1, LOW);
+
+  //// Wire.begin();
+  //// Wire.setClock(400000); // standard value is 100000
+  //// Wire.beginTransmission(xx);
+
+  init_lcd();
+}
+
+
+
 // Zera a entrada depois do tratamento para receber a proxima
 void newEnter(){
   nchar = 0;  
@@ -72,37 +97,32 @@ void newEnter(){
 
 // Funcao que configura estado inicial do display LCD 
 void init_lcd(){
-  display.begin();
-  display.setContrast(50); //Ajusta o contraste do display
-  display.clear();   //Apaga o buffer e o display
-  display.setTextSize(1);  //Seta o tamanho do texto
-  display.setTextColor(BLACK); //Seta a cor do texto
-  display.setCursor(0, 0);  //Seta a posição do cursor
-  display.println("PARADO");
-  display.setCursor(0,8);
-  display.println("DUTYCYCLE 000%");
-  display.setCursor(0,16);
-  display.println("VEL XXX RPM");
-  display.display();
+  lcd.begin(16, 2);
+  lcd.clear();   //Apaga o buffer e o display
+  lcd.setCursor(0, 0);  //Seta a posição do cursor
+  lcd.println("PARADO");
+  lcd.setCursor(0,8);
+  lcd.println("DUTYCYCLE 000%");
+  lcd.setCursor(0,16);
+  lcd.println("VEL XXX RPM");
+  lcd.display();
 
 }
 
 // Exibe na segunda linha do visor a velocidade atual do motor
 void LCDVel (String mostra) {
-  display.fillRect(0x18, 0x10, 0x11, 0x8, WHITE);
-  display.setCursor(0x18, 0x10);
-  display.println(mostra);
-  display.println(" %");
-  display.display();
+  lcd.setCursor(0x18, 0x10);
+  lcd.println(mostra);
+  lcd.println(" aa%");
+  lcd.display();
   
 }
 
 // Exibe na primeira linha do visor o estado atual do motor
 void LCDEstado (String mostra) {
-  display.fillRect(0x0, 0x0, 0x50, 0x8, WHITE);
-  display.setCursor(0, 0);
-  display.println(mostra);
-  display.display();
+  lcd.setCursor(0, 0);
+  lcd.println(mostra);
+  lcd.display();
 
 }
 
@@ -157,30 +177,6 @@ void erro(int codigo) {
 
 
 
-/***    ***/
-
-void setup(){
-  Serial.begin(9600);   // taxa de transmissao serial
-  /// bluetooth.begin(9600);
-
-  // Seta como output os pinos de pwm e pinos ponte H
-  pinMode(pinPWM, OUTPUT);
-  pinMode(pinH1, OUTPUT);
-  pinMode(pinH0, OUTPUT);
-
-  // Deixa parado inicialmente
-  digitalWrite(pinH0, LOW);
-  digitalWrite(pinH1, LOW);
-
-  //// Wire.begin();
-  //// Wire.setClock(400000); // standard value is 100000
-  //// Wire.beginTransmission(xx);
-
-  init_lcd();
-
-}
-
-
 
 void loop() {
 
@@ -189,47 +185,49 @@ void loop() {
     
     /// input = input + char(bluetooth.read()); // comuta string ate o fim do comando
     input = input + char(Serial.read()); // comuta string ate o fim do comando
-    last = input[nchar];
+    char last = input[nchar];
 
     if (last == '*') { // usar charcmp
       Serial.print("TESTE - fim de linha");
 
-      if (strstr(input, "VEL")) {
-        substring(input, velo, 4, 3); // create velo as a substring of input starting from 5 and w size of 3
+      if (input.substring(0,3) == "VEL") {
+        velo = input.substring(5, 8); // create velo as a substring of input starting from 5 and w size of 3
        
-        if (isDigit(velo)) { // check if the first digit of char velo is a number
+        for (int i = 0; i < 2; i++) {
+          if (!(isDigit(velo[i]))) erro(2); // caso nao tenha um valor numerico como parametro
+
           int newVel = velo.toInt();  // conver the string velo into a integer
           if (newVel < 0 || newVel > 100) erro(3); // caso nao seja um valor certo
           else {
             setVel(newVel);   // sendo um valor valido, seta a velo para este novo valor
-            LCDVel(newVel);
-            Serial.println("Nova vel:", newVel);
+            LCDVel(velo);
+            Serial.print("Nova vel: ");
+            Serial.println(velo);
             exibe7seg(newVel);
           } 
 
-        } else erro(2); // caso nao tenha um valor numerico como parametro
+        }
 
 
-
-      } else if (stringcmp(input, "VENT*")) { // se o comando for VENT
+      } else if (input == "VENT*") { // se o comando for VENT
           setFlow(1);
           LCDEstado("VENTILADOR");
-          Serial.print("OK VENT");
+          Serial.println("OK VENT");
 
-      } else if (stringcmp(input, "EXAUST*")) {
+      } else if (input == "EXAUST*") {
           setFlow(2);
           LCDEstado("EXAUSTOR");
-          Serial.print("OK EXAUST");
+          Serial.println("OK EXAUST");
 
-      } else if (stringcmp(input, "PARA*")) {
+      } else if (input == "PARA*") {
           setFlow(0);
           LCDEstado("PARADO");
-          Serial.print("OK PARA");
+          Serial.println("OK PARA");
 
-      } else if (stringcmp(input, "RETVEL*")) {
+      } else if (input == "RETVEL*") {
           Serial.print("VEL: ");
           Serial.print(rpm); // valor medido no pwm
-          Serial.print(" RPM");  
+          Serial.println(" RPM");  
 
       } else erro(1); 
     }
